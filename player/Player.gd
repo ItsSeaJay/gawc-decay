7b624c1a9802589ed5ae7563e2aa0d1f8bc5ce36
Callum John <callum@sea-jay.net>
2020-05-17 18:33:57 +0100

    Setup project for pixel art

--- player/Player.gd
+++ player/Player.gd
@@ -15,17 +15,20 @@ export var terminal_velocity = 128
 
 var state = State.STATE_NORMAL
 var gravity
+var jump_speed
 
 onready var velocity = Vector2.ZERO
 onready var velocity_final = Vector2.ZERO
 
 func _ready():
 	gravity = 2 * jump_height / pow(jump_duration, 2)
+	jump_speed = -sqrt(2 * gravity * jump_height)
 
 func _physics_process(delta):
 	match(state):
 		State.STATE_NORMAL:
 			process_movement()
+			process_jumping()
 			
 			if not is_on_floor():
 				transition(State.STATE_AIRBORNE)
@@ -40,6 +43,10 @@ func _physics_process(delta):
 
 func process_movement():
 	velocity.x = get_input_direction() * movement_speed
+
+func process_jumping():
+	if Input.is_action_just_pressed("move_jump"):
+		velocity.y = -jump_speed
 
 func process_falling():
 	velocity.y = min(velocity.y + gravity, terminal_velocity)
