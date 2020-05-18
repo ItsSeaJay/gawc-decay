35dc18240942ac3d7cca4970f4f2d81b7a09f77f
Callum John <callum@sea-jay.net>
2020-05-18 07:49:26 +0100

    Fix falling calculation

--- player/Player.gd
+++ player/Player.gd
@@ -10,7 +10,7 @@ enum State {
 
 export var movement_speed = 64
 export var jump_height = 32
-export var jump_duration = 0.48
+export var jump_duration = 2
 export var terminal_velocity = 128
 
 var state = State.STATE_NORMAL
@@ -21,35 +21,38 @@ onready var velocity = Vector2.ZERO
 onready var velocity_final = Vector2.ZERO
 
 func _ready():
-	gravity = 2 * jump_height / pow(jump_duration, 2)
-	jump_speed = -sqrt(2 * gravity * jump_height)
+	gravity = 2.0 * jump_height / pow(jump_duration / 2.0, 2.0)
+	jump_speed = -sqrt(2.0 * gravity * jump_height)
 
 func _physics_process(delta):
 	match(state):
 		State.STATE_NORMAL:
-			process_movement()
-			process_jumping()
+			process_movement(delta)
+			process_jumping(delta)
 			
 			if not is_on_floor():
 				transition(State.STATE_AIRBORNE)
 		State.STATE_AIRBORNE:
-			process_movement()
-			process_falling()
+			process_movement(delta)
+			process_falling(delta)
 			
 			if is_on_floor():
 				transition(State.STATE_NORMAL)
 	
-	velocity_final = move_and_slide(velocity)
+	velocity_final = move_and_slide(velocity, Vector2.UP)
+	
+	print(state)
 
-func process_movement():
+func process_movement(delta):
 	velocity.x = get_input_direction() * movement_speed
 
-func process_jumping():
+func process_jumping(delta):
 	if Input.is_action_just_pressed("move_jump"):
-		velocity.y = -jump_speed
+		velocity.y = jump_speed
 
-func process_falling():
-	velocity.y = min(velocity.y + gravity, terminal_velocity)
+func process_falling(delta):
+	velocity.y = min(velocity.y + gravity * delta, terminal_velocity)
+	pass
 
 func transition(state):
 	match(state):
